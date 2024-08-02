f92cde19eb701cb24cd364b0a64cb5a289c2ce10
xsonykkx <sofiya.strologo@gmail.com>
2024-08-02 18:31:17 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,9 @@
+def divide (first, second):
+    if second == 0:
+        print("Ошибка")
+    else:
+        return(first / second)
+
+
+
+divide(6,3)
---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from true_math import divide as true_dv
+from fake_math import divide as fake_dv
+
+result1 = fake_dv(69, 3)
+result2 = fake_dv(3, 0)
+result3 = true_dv(49, 7)
+result4 = true_dv(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
